d9b6ee1844bafc62edf7ec9c7402a0b54487de85
pete <pete@latebloomer.studio>
2023-06-16 15:05:07 -0400

    boilerplate

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
+import Lenis from '@studio-freight/lenis'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import $ from 'jquery'
 
-console.log('Welcome to Vite + JS + Webflow!')
+// WINDOW RESIZE LISTENER
+let isDesktop = window.matchMedia('(min-width: 768px)').matches
+window.addEventListener('resize', () => {
+  isDesktop = window.matchMedia('(min-width: 768px)').matches
+})
+// ===============
 
-createBadge()
-animateTitle()
+gsap.registerPlugin(ScrollTrigger) // GSAP SETUP
+// LENIS SETUP
+const lenis = new Lenis({ lerp: 0.05 })
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
+// ===============
+
+// GSAP ANIMATIONS
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: 'html',
+      start: 'top top-=100',
+      scrub: true,
+    },
+  })
+  .from('body', {
+    color: 'red',
+  })
+// ===============
+console.log($('body'), isDesktop)
